a7ee6865414c6d1fbda759f36e740b9d8364f506
Thomas <totoalexandre6259@gmail.com>
2023-10-25 18:44:17 +0200

    add BalisesSection Hero section Ok

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,13 @@
 "use server";
 import { Hero } from "@/components/features/Hero";
-
+import { BalisesSection } from "@/components/features/BalisesSection";
 export default async function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <div className="h-screen bg-primary">
+        <BalisesSection />
+      </div>
+    </>
+  );
 }
