ac4fee985026889608d9141e2ed7a81e601d4793
Thomas <totoalexandre6259@gmail.com>
2023-10-28 19:12:02 +0200

    BalisesTab OK adapte padding and change title card

--- src/components/features/BalisesTab.tsx
+++ src/components/features/BalisesTab.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export const BalisesTab = () => {
   return (
     <div className="h-1/2-screen w-[25%] relative overflow-y-scroll no-scrollbar bg-gray-300 shadow-2xl rounded-3xl">
-      <div className="bg-gray-200 static w-full h-[10%] rounded-t-3xl flex justify-center items-center font-montserrat text-xl text-secondary font-bold shadow-xl ">
-        Our different spots
+      <div className="bg-gray-200 uppercase z-40 w-full h-[10%] rounded-t-3xl flex justify-center items-center font-montserrat text-xl text-secondary font-bold shadow-xl ">
+        Spots
       </div>
       <table className="w-full h-auto mt-4 ">
         <thead>
@@ -12,9 +12,9 @@ export const BalisesTab = () => {
             <th></th>
           </tr>
         </thead>
-        <tBody className="flex flex-col items-center justify-center gap-4">
+        <tbody className="flex flex-col items-center justify-center gap-4 pb-4">
           <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
+            <td className="px-2 text-center">Mont des htreherthrt cats</td>
           </tr>
           <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
             <td className="">Mont des cats</td>
@@ -88,7 +88,7 @@ export const BalisesTab = () => {
           <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
             <td className="">Mont des cats</td>
           </tr>
-        </tBody>
+        </tbody>
       </table>
     </div>
   );
