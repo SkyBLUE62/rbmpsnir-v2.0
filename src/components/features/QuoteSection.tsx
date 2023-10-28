36107388d78b3a97f07afcf93d3ebe7ce1a6e79b
Thomas <totoalexandre6259@gmail.com>
2023-10-28 19:12:12 +0200

    Init Quote Section

---
+++ src/components/features/QuoteSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const QuoteSection = () => {
+  return (
+    <div className="h-screen bg-secondary">
+      <div className="max-w-9xl mx-auto h-full bg-red-500 py-4">
+        <div className="flex items-center justify-center">Our beacons</div>
+      </div>
+    </div>
+  );
+};
