e2b51aed143fcbd80b492babb1b463250619febd
Thomas <totoalexandre6259@gmail.com>
2023-10-31 22:22:56 +0100

    add Analytics components div main => relative

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,12 +2,14 @@
 import { Hero } from "@/components/features/Hero";
 import { BalisesSection } from "@/components/features/BalisesSection";
 import { QuoteSection } from "@/components/features/QuoteSection";
+import { Analytics } from "@/components/features/Analytics";
 export default async function Home() {
   return (
     <>
       <Hero />
-      <div className="h-screen bg-primary">
+      <div className="h-screen bg-primary relative">
         <BalisesSection />
+        <Analytics />
       </div>
       <QuoteSection />
     </>
