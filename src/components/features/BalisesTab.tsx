1e65c7db5f6c4a4c7d0a768fba4de17e11126567
Thomas <totoalexandre6259@gmail.com>
2023-11-02 20:28:26 +0100

    list beacon with visible => true

--- src/components/features/BalisesTab.tsx
+++ src/components/features/BalisesTab.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import type { Balise } from "@prisma/client";
 
-export const BalisesTab = () => {
+type Props = {
+  beacons: Balise[];
+};
+
+export const BalisesTab = ({ beacons }: Props) => {
   return (
     <div className="h-1/2-screen w-[25%] relative overflow-y-scroll no-scrollbar bg-gray-300 shadow-2xl rounded-3xl">
       <div className="bg-gray-200 uppercase z-40 w-full h-[10%] rounded-t-3xl flex justify-center items-center font-montserrat text-xl text-secondary font-bold shadow-xl ">
@@ -13,81 +17,14 @@ export const BalisesTab = () => {
           </tr>
         </thead>
         <tbody className="flex flex-col items-center justify-center gap-4 pb-4">
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="px-2 text-center">Mont des htreherthrt cats</td>
-          </tr>
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>{" "}
-          <tr className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold">
-            <td className="">Mont des cats</td>
-          </tr>
+          {beacons.map(({ lieu }, index) => (
+            <tr
+              className="w-1/2 h-14 bg-transparent border-2 border-[#131212] rounded-2xl flex items-center justify-center text-secondary font-montserrat font-semibold"
+              key={index}
+            >
+              <td className="px-2 text-center">{lieu}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
