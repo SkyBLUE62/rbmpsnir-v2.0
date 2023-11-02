d185c7effa21bd853f64a359cffc01676e336f42
Thomas <totoalexandre6259@gmail.com>
2023-11-02 20:27:33 +0100

    type script server component erreur

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,11 +3,13 @@ import { Hero } from "@/components/features/Hero";
 import { BalisesSection } from "@/components/features/BalisesSection";
 import { QuoteSection } from "@/components/features/QuoteSection";
 import { Analytics } from "@/components/features/Analytics";
+
 export default async function Home() {
   return (
     <>
       <Hero />
       <div className="h-screen bg-primary relative">
+        {/* @ts-expect-error Server Component */}
         <BalisesSection />
         <Analytics />
       </div>
