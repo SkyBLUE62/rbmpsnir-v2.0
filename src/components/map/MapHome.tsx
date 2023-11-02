66cc68a1c90be757cd9e95af5a2fa46e4dc4e857
Thomas <totoalexandre6259@gmail.com>
2023-11-02 20:30:42 +0100

    add filter difficulty and filter tab props beacons => add marker

--- src/components/map/MapHome.tsx
+++ src/components/map/MapHome.tsx
@@ -5,6 +5,7 @@ import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
+import { Balise } from "@prisma/client";
 
 const MapContainer = dynamic(
   () => import("react-leaflet").then((module) => module.MapContainer),
@@ -31,12 +32,19 @@ const Popup = dynamic(
   }
 );
 
+import { useFilterDifficulty } from "@/store/filterDifficultyMap";
+
 import expert from "../../../public/assets/images/markers/expert.svg";
 import intermediate from "../../../public/assets/images/markers/intermediate.svg";
 import beginner from "../../../public/assets/images/markers/beginner.svg";
 
-export const MapHome = () => {
+type Props = {
+  beacons: Balise[];
+};
+
+export const MapHome = ({ beacons }: Props) => {
   const [ready, setReady] = useState<boolean>(false);
+  const { difficultyFilter } = useFilterDifficulty();
 
   useEffect(() => {
     setReady(true);
@@ -47,16 +55,46 @@ export const MapHome = () => {
     iconSize: [32, 32],
   });
 
+  const difficulteIcon = (difficulte: string) => {
+    switch (difficulte) {
+      case "beginner":
+        return new L.Icon({
+          iconUrl: beginner.src,
+          iconSize: [32, 32],
+        });
+      case "intermediate":
+        return new L.Icon({
+          iconUrl: intermediate.src,
+          iconSize: [32, 32],
+        });
+      case "expert":
+        return new L.Icon({
+          iconUrl: expert.src,
+          iconSize: [32, 32],
+        });
+    }
+  };
+
   return (
     ready && (
       <div className="h-1/2-screen w-2/3 bg-gray-300 shadow-2xl rounded-3xl relative overflow-hidden">
         <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          <Marker position={[51.505, -0.09]} icon={ExpertIcon}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
+
+          {beacons
+            .filter(
+              ({ difficulte }) =>
+                difficulte === difficultyFilter || difficultyFilter === "reset"
+            )
+            .map(({ latitude, longitude, lieu, difficulte }, index) => (
+              <Marker
+                key={index}
+                position={[latitude, longitude]}
+                icon={difficulteIcon(difficulte)}
+              >
+                <Popup>{lieu}</Popup>
+              </Marker>
+            ))}
         </MapContainer>
       </div>
     )
