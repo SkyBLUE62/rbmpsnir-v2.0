4145a9a62dad64688718fef07e72c64698acb2eb
Thomas <totoalexandre6259@gmail.com>
2023-11-02 20:31:07 +0100

    navIsOpen store zustand

---
+++ src/store/navIsOpen.ts
@@ -0,0 +1,13 @@
+import { create } from 'zustand';
+
+type IsOpenStore = {
+  isOpen: boolean,
+  changeIsOpen: (state: boolean) => void;
+}
+
+const useNavIsOpen = create<IsOpenStore>((set) => ({
+  isOpen: false,
+  changeIsOpen: () => set((state) => ({ isOpen: !state.isOpen })),
+}));
+
+export { useNavIsOpen };
