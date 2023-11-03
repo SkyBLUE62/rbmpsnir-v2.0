28b9f683084eb1a419a2fc92d04d939e96a28b3f
Thomas <totoalexandre6259@gmail.com>
2023-11-03 21:24:57 +0100

    nav animation ok => animate text nav

--- src/components/features/layout/Nav.tsx
+++ src/components/features/layout/Nav.tsx
@@ -1,20 +1,44 @@
 "use client";
+
 import { useNavIsOpen } from "@/store/navIsOpen";
-import { useEffect } from "react";
+import { gsap } from "gsap";
+import { useEffect, useRef, useState } from "react";
 
 export const Nav = () => {
   const { isOpen } = useNavIsOpen();
+  const containerNav = useRef<HTMLDivElement>(null);
+
+  const [renderNav, setRenderNav] = useState<boolean>(false);
 
   useEffect(() => {
     if (isOpen) {
       document.body.style.overflow = "hidden";
+      containerNav.current?.classList.remove("hidden");
+      
+      gsap.fromTo(
+        containerNav.current,
+        { width: 0, height: 0 },
+        { width: "100%", height: "100vh", duration: 0.5 }
+      );
     } else {
       document.body.style.overflow = "auto";
+      gsap.fromTo(
+        containerNav.current,
+        { width: "100%", height: "100vh" },
+        { width: "0%", height: "0%", duration: 0.5 }
+      );
+
+      setTimeout(() => {
+        containerNav.current?.classList.add("hidden");
+      }, 1000);
     }
   }, [isOpen]);
 
-  return isOpen ? (
-    <div className="absolute top-0 right-0 h-screen w-screen bg-[#000000] z-50">
+  return (
+    <div
+      ref={containerNav}
+      className="absolute top-0 hidden right-0 h-screen w-screen bg-[#000000] z-50"
+    >
       <nav className="h-full w-full flex flex-row gap-36 justify-center items-center font-montserrat text-4xl text-primary font-semibold">
         <ul className="flex flex-col gap-16 h-1/5">
           <li>Home</li>
@@ -31,5 +55,5 @@ export const Nav = () => {
         </ul>
       </nav>
     </div>
-  ) : null;
+  );
 };
