a91d011dfb094f6c716642f4948f733d49b7505c
Thomas <totoalexandre6259@gmail.com>
2023-11-07 20:07:05 +0100

    config daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,48 +1,46 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundColor: {
-        'primary':"#131212",
-        'secondary':"#E6E6E6",
+        primary: "#131212",
+        secondary: "#E6E6E6",
       },
       colors: {
-        'primary':"#E6E6E6",
-        'secondary':"#131212",
+        primary: "#E6E6E6",
+        secondary: "#131212",
       },
       fontSize: {
-        '11xl': '10rem'
+        "11xl": "10rem",
       },
       fontFamily: {
-        'montserrat': ['Montserrat'],
-        'monument': ['MonumentExtended'],
+        montserrat: ["Montserrat"],
+        monument: ["MonumentExtended"],
       },
       maxWidth: {
-        '9xl':'100rem'
+        "9xl": "100rem",
       },
       backgroundImage: {
-        'white-black': 'linear-gradient(90deg, #E6E6E6 0%, #131212 100%)',
+        "white-black": "linear-gradient(90deg, #E6E6E6 0%, #131212 100%)",
       },
       height: {
-        '1/2-screen': '50vh',
-        '1/4-screen': '25vh',
-        '2/3-screen': '66vh',
-        '3/4-screen': '75vh',
+        "1/2-screen": "50vh",
+        "1/4-screen": "25vh",
+        "2/3-screen": "66vh",
+        "3/4-screen": "75vh",
       },
       zIndex: {
-        60: '60',
-        70: '70',
-      }
+        60: "60",
+        70: "70",
+      },
     },
   },
-  plugins: [
-    
-  ],
-}
-export default config
+  plugins: [require("daisyui")],
+};
+export default config;
