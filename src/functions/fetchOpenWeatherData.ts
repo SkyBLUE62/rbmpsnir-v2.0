2438abc88b56eef74ef7f95e6af90a3ec2a7efcc
Thomas <totoalexandre6259@gmail.com>
2023-11-08 19:46:33 +0100

    add fetchHistoryLastDay fetch function

--- src/functions/fetchOpenWeatherData.ts
+++ src/functions/fetchOpenWeatherData.ts
@@ -1,12 +1,34 @@
-type params = {
+type fetchOpenWeatherData = {
   longitude: number;
   latitude: number;
 };
 
-export const fetchOpenWeatherData = async ({ longitude, latitude }: params) => {
+type fetchHistoryLastDay = {
+  longitude: number;
+  latitude: number;
+  start: number;
+};
+
+export const fetchOpenWeatherData = async ({
+  longitude,
+  latitude,
+}: fetchOpenWeatherData) => {
   console.log("new fetch");
   return await fetch(
     `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely&units=metric&appid=${process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY}`,
+    {
+      next: { revalidate: 3600 },
+    }
+  ).then((res) => res.json());
+};
+
+export const fetchHistoryLastDay = async ({
+  latitude,
+  longitude,
+  start,
+}: fetchHistoryLastDay) => {
+  return await fetch(
+    `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${latitude}&lon=${longitude}&dt=${start}&exclude=minutely&units=metric&appid=${process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY}`,
     {
       next: { revalidate: 3600 },
     }
