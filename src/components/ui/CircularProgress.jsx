43938e81c420ee812defd0a29aaaad0989f20da3
Thomas <totoalexandre6259@gmail.com>
2023-11-08 19:45:12 +0100

    remove daisyui component

--- src/components/ui/CircularProgress.jsx
+++ src/components/ui/CircularProgress.jsx
@@ -4,13 +4,10 @@ import React from "react";
 
 export default function CircularProgressTemplate({ value, unit }) {
   return (
-    <div
-      className="radial-progress absolute left-1/2 top-[55%] transform -translate-x-1/2 -translate-y-1/2 text-primary"
-      style={{ "--value": "70", "--size": "6rem", "--thickness": "4px" }}
-    >
-      <div className="flex flex-col items-center justify-center">
-        <span>{value}</span>
-        <span>{unit}</span>
+    <div className="relative w-full h-full">
+      <div className="h-24 w-24 flex flex-col items-center justify-center border-2 border-primary rounded-full absolute left-1/2 top-1/3 transform -translate-x-1/2 -translate-y-1/2">
+        <span className="text-xl font-monument text-primary">{value}</span>
+        <span className="text-sm font-monument text-primary">{unit}</span>
       </div>
     </div>
   );
