e3f9d445d75679e921830508f2641a697d1ac080
Thomas <totoalexandre6259@gmail.com>
2023-11-08 19:44:07 +0100

    div restructuration

--- src/components/features/layout/Header.tsx
+++ src/components/features/layout/Header.tsx
@@ -1,11 +1,14 @@
 "use server";
 import { BtnNav } from "./uiNav/BtnNav";
 import { MainLogo } from "@/components/icons/MainLogo";
+
 export const Header = () => {
   return (
-    <div className="z-60 h-[10vh]  max-w-9xl mx-auto w-full flex flex-row items-center justify-between">
-      <MainLogo />
-      <BtnNav />
+    <div className="h-[10vh] w-full bg-primary shadow-black shadow-lg">
+      <div className="max-w-9xl h-full mx-auto w-full flex flex-row items-center justify-between">
+        <MainLogo />
+        <BtnNav />
+      </div>
     </div>
   );
 };
