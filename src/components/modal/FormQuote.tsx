0a672d98f2a7efa9d32aa1ae7b284f5b1e1d3678
Thomas <totoalexandre6259@gmail.com>
2023-11-11 19:46:39 +0100

    formQuote component form

--- src/components/modal/FormQuote.tsx
+++ src/components/modal/FormQuote.tsx
@@ -1,12 +1,32 @@
-import React from "react";
-
+"use client";
+import type { TypeBalise } from "@prisma/client";
 type FormQuoteProps = {
-  idType: string;
+  beaconDetail: TypeBalise;
 };
-import { useModalFormQuote } from "@/store/modalFormQuote";
 
-export const FormQuote = ({ idType }: FormQuoteProps) => {
-  const { isOpen } = useModalFormQuote();
-
-  return isOpen && <div>FormQuote</div>;
+export const FormQuote = ({ beaconDetail }: FormQuoteProps) => {
+  console.log(beaconDetail);
+  return (
+    <div className="fixed  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/2 h-1/2 bg-primary rounded-3xl z-70 text-white">
+      <div className="py-4 flex items-center justify-center">
+        <span>Formulaire de devis</span>
+      </div>
+      <form className="w-full h-full px-4 py-4">
+        <div className="flex flex-row justify-around items-center gap-8">
+          <div className="flex flex-col w-full ">
+            <label htmlFor=""></label>
+            <input
+              type="text"
+              placeholder={beaconDetail.name}
+              value={beaconDetail.name}
+            />
+          </div>
+          <div className="flex flex-col w-full ">
+            <label htmlFor=""></label>
+            <input type="text" />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
