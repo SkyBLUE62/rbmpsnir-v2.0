6601dca35cda80a5330f0c8d7cf1275a6872df87
Thomas <totoalexandre6259@gmail.com>
2023-11-11 19:46:23 +0100

    quote section open modal ok form ok

--- src/components/features/QuoteSection.tsx
+++ src/components/features/QuoteSection.tsx
@@ -1,11 +1,22 @@
+"use client";
+
 import { TypeBalise } from "@prisma/client";
 import Image from "next/image";
-
+import { FormQuote } from "../modal/FormQuote";
+import { BtnFormQuote } from "../ui/BtnFormQuote";
+import { useState } from "react";
 type Props = {
   typeBeacons: TypeBalise[];
 };
 
 export const QuoteSection = ({ typeBeacons }: Props) => {
+  const [openModalId, setOpenModalId] = useState<string | null>(
+    "3748c34a-6a10-4e95-84a9-f8648a4c3b05"
+  );
+  const openModal = (id: string) => {
+    setOpenModalId(id);
+    document.body.style.overflow = "hidden";
+  };
   return (
     <div className="h-screen mt-[20vh] flex items-center bg-secondary">
       <div className="max-w-9xl mx-auto h-full flex flex-col gap-16 font-montserrat text-primary items-center justify-center ">
@@ -13,12 +24,11 @@ export const QuoteSection = ({ typeBeacons }: Props) => {
           Our beacons offers
         </span>
         <div className="grid grid-cols-4 gap-16 gap-y-6 h-auto w-full py-10">
-          {typeBeacons.map(({ name, image, slug, description }, index) => (
+          {typeBeacons.map(({ name, image, id, description }, index) => (
             <div
               key={index}
               className="h-full w-full bg-primary rounded-3xl shadow-black shadow-2xl mb-4 p-4"
             >
-
               <div className="w-full flex items-center justify-center">
                 <Image
                   src={"/assets/images/balises/" + image}
@@ -35,15 +45,10 @@ export const QuoteSection = ({ typeBeacons }: Props) => {
                 <div className="flex items-center justify-center text-center opacity-60 text-base">
                   {description}
                 </div>
-                <button className="bg-orange-600 h-auto w-3/4 py-2 px-2 rounded-2xl flex flex-row items-center justify-center gap-2">
-                  Request a quote
-                  <Image
-                    src={`/assets/images/icons/greater-than.svg`}
-                    alt={"greater than"}
-                    width={20}
-                    height={20}
-                  />
-                </button>
+                <BtnFormQuote id={id} setOpenModalId={() => openModal(id)} />
+                {openModalId === id && (
+                  <FormQuote beaconDetail={typeBeacons[index]} />
+                )}
               </div>
             </div>
           ))}
