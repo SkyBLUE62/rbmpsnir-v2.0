721847eab17bbace934395d345c198809e202524
Thomas <totoalexandre6259@gmail.com>
2023-11-13 21:32:39 +0100

    add difficulte fonction

--- src/components/map/MapHome.tsx
+++ src/components/map/MapHome.tsx
@@ -2,11 +2,11 @@
 
 import "./index.css";
 import "leaflet/dist/leaflet.css";
-import L from "leaflet";
+
 import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import { Balise } from "@prisma/client";
-
+import { difficulteIcon } from "@/functions/difficulteIcon";
 const MapContainer = dynamic(
   () => import("react-leaflet").then((module) => module.MapContainer),
   {
@@ -34,10 +34,6 @@ const Popup = dynamic(
 
 import { useFilterDifficulty } from "@/store/filterDifficultyMap";
 
-import expert from "../../../public/assets/images/markers/expert.svg";
-import intermediate from "../../../public/assets/images/markers/intermediate.svg";
-import beginner from "../../../public/assets/images/markers/beginner.svg";
-
 type Props = {
   beacons: Balise[];
 };
@@ -47,37 +43,14 @@ export const MapHome = ({ beacons }: Props) => {
   const { difficultyFilter } = useFilterDifficulty();
 
   useEffect(() => {
-    setReady(true);
-  }, []);
-
-  const ExpertIcon = new L.Icon({
-    iconUrl: beginner.src,
-    iconSize: [32, 32],
-  });
-
-  const difficulteIcon = (difficulte: string) => {
-    switch (difficulte) {
-      case "beginner":
-        return new L.Icon({
-          iconUrl: beginner.src,
-          iconSize: [32, 32],
-        });
-      case "intermediate":
-        return new L.Icon({
-          iconUrl: intermediate.src,
-          iconSize: [32, 32],
-        });
-      case "expert":
-        return new L.Icon({
-          iconUrl: expert.src,
-          iconSize: [32, 32],
-        });
+    if (typeof window !== "undefined") {
+      setReady(true);
     }
-  };
+  }, []);
 
   return (
     ready && (
-      <div className="h-1/2-screen w-2/3 bg-gray-300 shadow-2xl rounded-3xl relative overflow-hidden">
+      <div className="h-1/2-screen w-2/3 bg-gray-300 shadow-2xl rounded-3xl z-10 relative overflow-hidden">
         <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
