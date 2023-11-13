d27e9c5d1e3f24165016335f06f1a807bdcb5343
Thomas <totoalexandre6259@gmail.com>
2023-11-13 21:34:33 +0100

    id openweather to icon svg

--- src/functions/IDToIconOpenWeather.ts
+++ src/functions/IDToIconOpenWeather.ts
@@ -1,28 +1,21 @@
 export const IDToIconOpenWeather = (id: number) => {
-  switch (id) {
-    case id >= 200 && id <= 232:
-      return "/assets/images/icons/weather/thunderstorm.svg";
-      break;
-    case id >= 300 && id <= 321:
-      return "/assets/images/icons/weather/drizzle.svg";
-      break;
-    case id >= 500 && id <= 531:
-      return "/assets/images/icons/weather/rain.svg";
-      break;
-    case id >= 600 && id <= 622:
-      return "/assets/images/icons/weather/snow.svg";
-      break;
-    case id === 800:
-      return "/assets/images/icons/weather/clear-day.svg";
-      break;
-    case id === 801:
-      return "/assets/images/icons/weather/partly-cloudy.svg";
-      break;
-    case id === 802:
-      return "/assets/images/icons/weather/cloudy.svg";
-    case id === 803 && id === 804:
-      return "/assets/images/icons/weather/overcast.svg";
-    default:
-      break;
+  if (id >= 200 && id <= 232) {
+    return "/assets/images/icons/weather/thunderstorm.svg";
+  } else if (id >= 300 && id <= 321) {
+    return "/assets/images/icons/weather/drizzle.svg";
+  } else if (id >= 500 && id <= 531) {
+    return "/assets/images/icons/weather/rain.svg";
+  } else if (id >= 600 && id <= 622) {
+    return "/assets/images/icons/weather/snow.svg";
+  } else if (id === 800) {
+    return "/assets/images/icons/weather/clear-day.svg";
+  } else if (id === 801) {
+    return "/assets/images/icons/weather/partly-cloudy.svg";
+  } else if (id === 802) {
+    return "/assets/images/icons/weather/cloudy.svg";
+  } else if (id === 803 || id === 804) {
+    return "/assets/images/icons/weather/overcast.svg";
+  } else {
+    return "/assets/images/icons/weather/clear-day.svg";
   }
 };
