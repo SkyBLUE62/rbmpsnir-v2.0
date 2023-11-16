411afd1fa33827522749745521ef567feaf252ec
Thomas <totoalexandre6259@gmail.com>
2023-11-16 21:04:11 +0100

    responsive mobile

--- src/components/map/FiltersHome.tsx
+++ src/components/map/FiltersHome.tsx
@@ -7,8 +7,8 @@ export const FiltersHome = () => {
     useFilterDifficulty();
 
   return (
-    <div className="relative h-1/2-screen w-[4%] bg-gray-300 shadow-2xl rounded-full flex flex-col items-center justify-around">
-      <label className="relative inline-flex items-center cursor-pointer rotate-90">
+    <div className="relative md:h-1/2-screen md:w-[4%] h-12 w-full bg-gray-300 shadow-2xl rounded-full flex md:flex-col flex-row  items-center justify-around">
+      <label className="relative inline-flex items-center cursor-pointer md:rotate-90">
         <input
           type="checkbox"
           checked={difficultyFilter === "reset"}
@@ -18,7 +18,7 @@ export const FiltersHome = () => {
         <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
       </label>
 
-      <label className="relative inline-flex items-center cursor-pointer rotate-90">
+      <label className="relative inline-flex items-center cursor-pointer md:rotate-90">
         <input
           type="checkbox"
           value=""
@@ -29,7 +29,7 @@ export const FiltersHome = () => {
         <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-green-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
       </label>
 
-      <label className="relative inline-flex items-center cursor-pointer rotate-90">
+      <label className="relative inline-flex items-center cursor-pointer md:rotate-90">
         <input
           type="checkbox"
           value=""
@@ -40,7 +40,7 @@ export const FiltersHome = () => {
         <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-orange-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-orange-600"></div>
       </label>
 
-      <label className="relative inline-flex items-center cursor-pointer rotate-90">
+      <label className="relative inline-flex items-center cursor-pointer md:rotate-90">
         <input
           type="checkbox"
           value=""
