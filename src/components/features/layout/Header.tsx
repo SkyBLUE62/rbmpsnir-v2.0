4f618ec0e9314fefc00c91dd48aadd1bb568731e
Thomas <totoalexandre6259@gmail.com>
2023-11-17 22:16:51 +0100

    responsive ipad + phone

--- src/components/features/layout/Header.tsx
+++ src/components/features/layout/Header.tsx
@@ -5,7 +5,7 @@ import { MainLogo } from "@/components/icons/MainLogo";
 export const Header = () => {
   return (
     <div className="h-[10vh] w-full bg-primary shadow-black shadow-lg ">
-      <div className="max-w-9xl h-full mx-auto w-full flex flex-row items-center justify-between px-4 md:px-0">
+      <div className="max-w-9xl h-full mx-auto w-full flex flex-row items-center justify-between px-4 lg:px-0 pt-4 lg:pt-0 ">
         <MainLogo />
         <BtnNav />
       </div>
