a4630f3c1f0bb5243324ba3d280ae500a07c26b3
Thomas <totoalexandre6259@gmail.com>
2023-11-17 22:16:39 +0100

    responsive ipad + phone

--- src/components/features/Hero.tsx
+++ src/components/features/Hero.tsx
@@ -7,21 +7,21 @@ export const Hero = () => {
       <div className="h-[100vh] w-1/2  bg-primary bg-cover bg-right-top bg-no-repeat absolute left-0 top-0 z-10"></div>
 
       <main className="h-[90vh] w-full relative max-w-9xl mx-auto">
-        <span className="absolute  z-40 top-1/4 left-[15%] md:text-7xl text-4xl font-monument font-bold text-primary">
+        <span className="absolute  z-40 top-1/4 left-[15%] lg:text-7xl text-4xl font-monument font-bold text-primary">
           The
         </span>
-        <h1 className="absolute  z-40 h-auto w-full top-[35%] flex items-center justify-center  left-0  md:text-11xl text-5xl font-monument font-bold  ">
+        <h1 className="absolute  z-40 h-auto w-full top-[35%] flex items-center justify-center  left-0  lg:text-11xl md:text-7xl text-5xl font-monument font-bold  ">
           <span className="text-primary ">RBM</span>
           <span className="bg-gradient-to-r from-[#E6E6E6] from-20% via-[#131212] to-[#131212] text-transparent bg-clip-text">
             P
           </span>
           <span className="text-secondary">SNIR</span>
         </h1>
-        <h2 className="absolute z-40 md:top-2/3 top-1/2 left-[60%] md:text-7xl text-4xl text-secondary font-monument font-bold">
+        <h2 className="absolute z-40 md:top-2/3 top-1/2 left-[60%] lg:text-7xl text-4xl text-secondary font-monument font-bold">
           V2.0
         </h2>
 
-        <p className="md:w-1/4 w-2/4 opacity-60 h-auto text-lg text-primary font-montserrat z-40 absolute md:bottom-36 bottom-5 left-2 md:left-0">
+        <p className="md:w-1/4 w-2/4 opacity-60 h-auto text-lg text-primary font-montserrat z-40 absolute lg:bottom-36 bottom-5 left-2 lg:left-0">
           The best weather application for informing paragliders about the
           weather conditions at the best spots in the world!
         </p>
