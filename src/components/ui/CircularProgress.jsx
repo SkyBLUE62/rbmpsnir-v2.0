700c128488c0dc03ede79add3f03d5d9f424e21f
Thomas <totoalexandre6259@gmail.com>
2023-11-18 17:15:02 +0100

    responsive

--- src/components/ui/CircularProgress.jsx
+++ src/components/ui/CircularProgress.jsx
@@ -4,9 +4,11 @@ import React from "react";
 
 export default function CircularProgressTemplate({ value, unit }) {
   return (
-    <div className="relative w-full h-full">
-      <div className="h-24 w-24 flex flex-col items-center justify-center border-2 border-primary rounded-full absolute left-1/2 top-1/3 transform -translate-x-1/2 -translate-y-1/2">
-        <span className="text-xl font-monument text-primary">{value}</span>
+    <div className="relative -top-2  w-full h-full">
+      <div className="md:h-24 md:w-24 w-20 h-20 flex flex-col items-center justify-center border-2 border-primary rounded-full absolute left-1/2 top-1/3 transform -translate-x-1/2 -translate-y-1/2">
+        <span className="md:text-xl text-base font-monument text-primary">
+          {value}
+        </span>
         <span className="text-sm font-monument text-primary">{unit}</span>
       </div>
     </div>
