2cf48f85033792e5bbc45b1bc5491d89dab40fab
Thomas <totoalexandre6259@gmail.com>
2023-11-18 17:13:41 +0100

    dynamic import map & responsive

--- src/app/beacons/[slug]/page.tsx
+++ src/app/beacons/[slug]/page.tsx
@@ -5,16 +5,27 @@ import {
 } from "@/functions/fetchOpenWeatherData";
 import prisma from "../../../../prisma/db/prisma";
 import type { Balise } from "@prisma/client";
+
 import { notFound } from "next/navigation";
+import dynamic from "next/dynamic";
+
 import Image from "next/image";
 import { WindDirectionGraph } from "@/components/graph/WindDirectionGraph";
 import { SunriseGraph } from "@/components/graph/SunriseGraph";
 import { PressureCard } from "@/components/graph/PressureCard";
 import { WeatherForecast } from "@/components/weather/WeatherForecast";
-import { DailyTempGraph } from "@/components/graph/DailyTempGraph";
 import { HourlyData } from "@/dto/openweather";
 import { DetailsBeaconMap } from "@/components/map/DetailsBeaconMap";
 import { IDToIconOpenWeather } from "@/functions/IDToIconOpenWeather";
+import { MapSkeleton } from "@/components/loading/MapSkeleton";
+
+const DailyTempGraph = dynamic(
+  () => import("@/components/graph/DailyTempGraph"),
+  {
+    ssr: false,
+    loading: () => <MapSkeleton />,
+  }
+);
 
 const page = async ({ params }: { params: { slug: string } }) => {
   let beaconData: Balise;
@@ -59,9 +70,9 @@ const page = async ({ params }: { params: { slug: string } }) => {
   const iconById = IDToIconOpenWeather(current.weather[0].id);
   return (
     <>
-      <div className="lg:h-[90vh] h-auto lg:px-0 px-4 w-full py-8 bg-primary bg-opacity-90">
+      <div className="xl:h-[90vh] h-auto lg:px-0 px-4 w-full py-8 bg-primary bg-opacity-90">
         <div className="h-full w-full max-w-9xl mx-auto">
-          <div className="grid lg:grid-cols-3 lg:grid-rows-2 grid-cols-1 place-items-center h-full w-full lg:gap-8 gap-y-4 ">
+          <div className="grid lg:grid-cols-3 lg:grid-rows-2 grid-cols-1 place-items-center h-full w-full xl:gap-8 gap-y-4 gap-8">
             <div className="grid lg:grid-cols-1 lg:grid-rows-1 h-full w-full gap-8 ">
               <div className="lg:col-span-1 col-span-2 h-full w-full bg-primary rounded-3xl shadow-lg shadow-black">
                 <h1 className="h-[12%] w-full bg-secondary text-secondary rounded-t-2xl flex items-center justify-center font-montserrat text-xl font-semibold">
@@ -74,15 +85,16 @@ const page = async ({ params }: { params: { slug: string } }) => {
                       alt="weather"
                       width={100}
                       height={100}
+                      className="lg:w-[100px] lg:h-[100px] h-[75px] w-[75px]"
                     ></Image>
-                    <span className="text-3xl font-monument text-primary font-light">
+                    <span className="lg:text-3xl text-sm font-monument text-primary font-light">
                       {current.temp.toFixed(0)} C°
                     </span>
                   </div>
 
                   <WindDirectionGraph current={current} />
 
-                  <div className="h-auto relative  rounded-3xl">
+                  <div className="h-auto relative  rounded-3xl place-items-center md:place-items-start">
                     <div className="w-full flex flex-row justify-start items-center">
                       <Image
                         src={"/assets/images/icons/weather/sunrise.svg"}
@@ -102,13 +114,13 @@ const page = async ({ params }: { params: { slug: string } }) => {
                 </div>
               </div>
             </div>
-            <div className=" col-span-2 h-auto relative bg-primary rounded-3xl shadow-lg shadow-black">
+            <div className="col-span-2 w-full lg:w-auto lg:h-full h-auto relative bg-primary rounded-3xl shadow-lg shadow-black">
               <WeatherForecast daily={daily} />
             </div>
-            <div className="col-span-2 relative h-auto bg-primary rounded-3xl shadow-lg shadow-black">
+            <div className="col-span-2 relative w-full h-96  bg-primary rounded-3xl shadow-lg shadow-black">
               <DailyTempGraph historyData={historyData} />
             </div>
-            <div className="col-span-1 relative h-auto bg-primary rounded-3xl shadow-lg shadow-black">
+            <div className="col-span-1 relative w-full h-96 bg-primary rounded-3xl shadow-lg shadow-black">
               <DetailsBeaconMap
                 latitude={latitude}
                 longitude={longitude}
