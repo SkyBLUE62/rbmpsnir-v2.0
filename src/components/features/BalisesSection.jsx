1e812c5093beae2728998f5ab912e7ea1923be36
Thomas <totoalexandre6259@gmail.com>
2023-11-22 14:37:45 +0100

    Final commit

--- src/components/features/BalisesSection.jsx
+++ src/components/features/BalisesSection.jsx
@@ -4,6 +4,8 @@ import dynamic from "next/dynamic";
 
 import { FiltersHome } from "../map/FiltersHome";
 import { BalisesTab } from "./BalisesTab";
+import { Reveals } from "../animations/Reveals";
+
 const MapHome = dynamic(() => import("../map/MapHome"), {
   ssr: false,
   loading: () => <MapSkeleton />,
@@ -21,12 +23,16 @@ export const BalisesSection = async () => {
 
   return (
     beacons && (
-      <div className="max-w-9xl lg:h-screen h-auto py-4 lg:py-0 px-4 lg:px-0 w-full">
-        <div className="py-10 flex flex-col justify-center items-center gap-24 h-full w-full">
-          <span className="flex items-center text-center text-gray-300 justify-center font-monument font-bold text-5xl">
-            The best spots in the world
-          </span>
-
+      <div
+        id="balises"
+        className="max-w-9xl lg:h-screen h-auto py-4 lg:py-0 px-4 lg:px-0 w-full mx-auto"
+      >
+        <div className="py-10 md:py-16 flex flex-col justify-center items-center gap-24 md:gap-0 h-full w-full">
+          <Reveals>
+            <span className="flex items-center text-center text-gray-300 justify-center font-monument font-bold text-5xl">
+              The best spots in the world
+            </span>
+          </Reveals>
           <div className="flex lg:flex-row flex-col gap-4 lg:gap-0 items-center w-full justify-around relative h-full">
             <FiltersHome />
             {/* @ts-expect-error Server Component */}
