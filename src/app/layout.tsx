961a318b56a9cb999c9425ed08028941b2320c96
Thomas <totoalexandre6259@gmail.com>
2023-11-22 15:10:47 +0100

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { ToastContainer } from "react-toastify";
 export const metadata: Metadata = {
   title: "rbmp-snir v2.0",
   description: "rbmp-snir v2.0",
+  keywords: "weather, paragliders, weather application",
 };
 
 export default function RootLayout({
@@ -17,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </head>
       <body>
         <div className="min-h-screen w-auto flex flex-col flex-1 relative overflow-x-hidden bg-secondary">
           <Header />
