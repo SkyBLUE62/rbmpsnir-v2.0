832e33d815300bc744e8e3b119f0b28366d2b175
Thomas <totoalexandre6259@gmail.com>
2023-11-22 14:37:40 +0100

    Final commit

--- src/components/features/Analytics.tsx
+++ src/components/features/Analytics.tsx
@@ -3,6 +3,7 @@
 import { Club, Balise } from "@prisma/client";
 import React from "react";
 import CountUp from "react-countup";
+import { Reveals } from "../animations/Reveals";
 
 export const Analytics = ({
   clubs,
@@ -14,33 +15,41 @@ export const Analytics = ({
   return (
     <div className="h-[20vh] w-full bg-primary shadow-2xl px-4 lg:px-0  absolute -skew-y-[3deg] z-50 md:-bottom-[10vh] -bottom-[15vh]">
       <div className="h-full max-w-9xl mx-auto flex flex-row justify-around items-center text-primary font-monument">
-        <div className="flex flex-col md:text-lg w-1/3 lg:w-auto gap-2 justify-center items-center lg:text-3xl text-xs">
-          Number of beacons
-          <CountUp
-            delay={2}
-            end={beacons.length}
-            duration={5}
-            className="lg:text-5xl md:text-lg text-xs"
-          />
-        </div>
-        <div className="flex flex-col md:text-lg w-1/3 lg:w-auto gap-4 justify-center items-center lg:text-3xl text-xs">
-          Member club
-          <CountUp
-            delay={2}
-            end={clubs.length}
-            duration={5}
-            className="lg:text-5xl md:text-lg text-xs"
-          />
-        </div>
-        <div className="flex w-1/3 flex-col md:text-lg lg:w-auto gap-4 justify-center items-center lg:text-3xl text-xs">
-          Mainland
-          <CountUp
-            delay={2}
-            end={4}
-            duration={5}
-            className="lg:text-5xl md:text-lg text-xs"
-          />
-        </div>
+        <Reveals>
+          <div className="flex flex-col md:text-lg w-1/3 lg:w-auto gap-2 justify-center items-center lg:text-3xl text-xs">
+            Number of beacons
+            <CountUp
+              delay={2}
+              end={beacons.length}
+              duration={5}
+              className="lg:text-5xl md:text-lg text-xs"
+            />
+          </div>
+        </Reveals>
+
+        <Reveals>
+          <div className="flex flex-col md:text-lg w-1/3 lg:w-auto gap-4 justify-center items-center lg:text-3xl text-xs">
+            Member club
+            <CountUp
+              delay={2}
+              end={clubs.length}
+              duration={5}
+              className="lg:text-5xl md:text-lg text-xs"
+            />
+          </div>
+        </Reveals>
+
+        <Reveals>
+          <div className="flex w-1/3 flex-col md:text-lg lg:w-auto gap-4 justify-center items-center lg:text-3xl text-xs">
+            Mainland
+            <CountUp
+              delay={2}
+              end={4}
+              duration={5}
+              className="lg:text-5xl md:text-lg text-xs"
+            />
+          </div>
+        </Reveals>
       </div>
     </div>
   );
