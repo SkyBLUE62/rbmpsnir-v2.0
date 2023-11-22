a34808a23c3a4c8b25d0fe709a4e56ce420f7075
Thomas <totoalexandre6259@gmail.com>
2023-11-22 14:39:14 +0100

    Add link popup + Final commit

--- src/components/map/MapHome.tsx
+++ src/components/map/MapHome.tsx
@@ -3,10 +3,11 @@
 import "./index.css";
 import "leaflet/dist/leaflet.css";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import dynamic from "next/dynamic";
 import { Balise } from "@prisma/client";
 import { difficulteIcon } from "@/functions/difficulteIcon";
+import { motion, useAnimation, useInView } from "framer-motion";
 
 const MapContainer = dynamic(
   () => import("react-leaflet").then((module) => module.MapContainer),
@@ -34,6 +35,7 @@ const Popup = dynamic(
 );
 
 import { useFilterDifficulty } from "@/store/filterDifficultyMap";
+import Link from "next/link";
 
 type Props = {
   beacons: Balise[];
@@ -43,39 +45,53 @@ const MapHome = ({ beacons }: Props) => {
   const [ready, setReady] = useState<boolean>(false);
   const { difficultyFilter } = useFilterDifficulty();
 
+  const ref = useRef<null | HTMLDivElement>(null);
+  const containerAnimation = useAnimation();
+  const isInView = useInView(ref, { once: true });
+
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      setReady(true);
+    if (isInView) {
+      containerAnimation.start("visible");
     }
-  }, []);
+  }, [isInView, containerAnimation]);
 
   return (
-    ready && (
-      <div className="h-1/2-screen lg:w-2/3 w-full bg-gray-300 shadow-2xl rounded-3xl z-10 relative overflow-hidden">
-        <MapContainer
-          center={[48.8566, 2.3522]}
-          zoom={2}
-          scrollWheelZoom={true}
-        >
-          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+    <motion.div
+      ref={ref}
+      variants={{
+        hidden: { opacity: 0, y: 75 },
+        visible: { opacity: 1, y: 0 },
+      }}
+      initial="hidden"
+      animate={containerAnimation}
+      transition={{ duration: 0.5, delay: 0.25 }}
+      className="h-1/2-screen lg:w-2/3 w-full bg-gray-300 shadow-2xl rounded-3xl z-10 relative overflow-hidden"
+    >
+      <MapContainer
+        center={[46.8566, 2.3522]}
+        zoom={2.3}
+        scrollWheelZoom={true}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-          {beacons
-            .filter(
-              ({ difficulte }) =>
-                difficulte === difficultyFilter || difficultyFilter === "reset"
-            )
-            .map(({ latitude, longitude, lieu, difficulte }, index) => (
-              <Marker
-                key={index}
-                position={[latitude, longitude]}
-                icon={difficulteIcon(difficulte)}
-              >
-                <Popup>{lieu}</Popup>
-              </Marker>
-            ))}
-        </MapContainer>
-      </div>
-    )
+        {beacons
+          .filter(
+            ({ difficulte }) =>
+              difficulte === difficultyFilter || difficultyFilter === "reset"
+          )
+          .map(({ latitude, longitude, lieu, difficulte, slug }, index) => (
+            <Marker
+              key={index}
+              position={[latitude, longitude]}
+              icon={difficulteIcon(difficulte)}
+            >
+              <Popup>
+                <Link href={`/beacons/${slug}`}>{lieu}</Link>
+              </Popup>
+            </Marker>
+          ))}
+      </MapContainer>
+    </motion.div>
   );
 };
 
