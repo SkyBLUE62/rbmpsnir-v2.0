1d0f7ba91cf09f1f2c110c22d6351d38bd56ffe4
Thomas <totoalexandre6259@gmail.com>
2023-11-22 14:38:07 +0100

    Final commit

--- src/components/features/layout/Header.tsx
+++ src/components/features/layout/Header.tsx
@@ -4,8 +4,8 @@ import { MainLogo } from "@/components/icons/MainLogo";
 
 export const Header = () => {
   return (
-    <div className="h-[10vh] sm:h-auto w-full bg-primary shadow-black shadow-lg ">
-      <div className="max-w-9xl  h-full mx-auto w-full flex flex-row items-center justify-between px-4 md:px-4 lg:px-4 xl:px-0 pt-4 lg:pt-0 ">
+    <div className="h-[10vh] w-full bg-primary shadow-black shadow-lg xl:px-4">
+      <div className="max-w-9xl h-full mx-auto w-full flex flex-row items-center justify-between px-4 md:px-4 lg:px-4 xl:px-0 pt-4 lg:pt-0 ">
         <MainLogo />
         <BtnNav />
       </div>
