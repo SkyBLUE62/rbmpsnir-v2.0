7dc57c34ba478d9c3548a1dc3a47c6e715276f1b
Thomas <totoalexandre6259@gmail.com>
2023-11-22 14:39:21 +0100

    Final commit

--- src/components/modal/FormQuote.tsx
+++ src/components/modal/FormQuote.tsx
@@ -2,6 +2,7 @@
 
 import { addQuoteAction } from "@/serverActions/Quote/addQuote.action";
 import type { TypeBalise } from "@prisma/client";
+import { motion } from "framer-motion";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
@@ -9,12 +10,27 @@ import "react-toastify/dist/ReactToastify.css";
 
 type FormQuoteProps = {
   beaconDetail: TypeBalise;
+  viewModal: boolean | null;
   closeModal: () => void;
 };
 
-export const FormQuote = ({ beaconDetail, closeModal }: FormQuoteProps) => {
+export const FormQuote = ({
+  beaconDetail,
+  closeModal,
+  viewModal,
+}: FormQuoteProps) => {
   return (
-    <div className="fixed top-1/2  left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:w-1/3 md:h-1/2 w-3/4 h-auto sm:w-full sm:h-full px-4 bg-primary rounded-3xl z-70 text-white border-primary border">
+    <motion.div
+      variants={{
+        visible: { opacity: 1, scale: 1, y: "-50%", x: "-50%" },
+        hidden: { opacity: 0, scale: 0, y: "-50%", x: "-50%" },
+      }}
+      initial="hidden"
+      animate={viewModal === true ? "visible" : "hidden"}
+      exit={"hidden"}
+      transition={{ duration: 0.5 }}
+      className="fixed  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 md:w-2/3 xl:w-1/3 lg:h-auto md:h-auto  xl:h-1/2  w-full h-auto sm:w-full sm:h-full px-4 bg-primary rounded-3xl z-70 text-white border-primary border"
+    >
       <div className="py-4 flex items-center justify-center bg-secondary rounded-t-3xl sm:flex md:flex  absolute w-full left-0">
         <span className="md:text-3xl font-monument font-semibold text-secondary">
           Quote form
@@ -31,10 +47,9 @@ export const FormQuote = ({ beaconDetail, closeModal }: FormQuoteProps) => {
 
       <div className="w-full h-[1px] bg-white"></div>
       <form
-        className="w-full h-full mt-14 md:mt-0 px-4 py-4 flex flex-col items-center justify-start gap-6"
+        className="w-full h-full mt-14 px-4 py-4 flex flex-col items-center justify-start gap-6"
         action={async (formAction: FormData) => {
           const status: any = await addQuoteAction(formAction);
-          console.log(status.issues);
           if (
             status.issues &&
             (status.status === "404" ||
@@ -109,9 +124,14 @@ export const FormQuote = ({ beaconDetail, closeModal }: FormQuoteProps) => {
           </div>
         </div>
 
-        <div className="flex sm:flex-row flex-col gap-2 md:items-start sm:items-center  justify-around w-full">
+        <div className="flex flex-col md:flex-row px-2 gap-2 md:items-start sm:items-center justify-around w-full">
           <div className="flex flex-col">
-            <label htmlFor="">Beacon</label>
+            <label
+              htmlFor=""
+              className="text-center sm:text-center md:text-start"
+            >
+              Beacon
+            </label>
             <textarea
               className="rounded-xl text-secondary px-2 py-1 resize-none focus:border focus:border-orange-600 outline-none"
               placeholder={beaconDetail.name}
@@ -120,8 +140,13 @@ export const FormQuote = ({ beaconDetail, closeModal }: FormQuoteProps) => {
               name="beacon"
             />
           </div>
-          <div className="flex flex-col sm:hidden">
-            <label htmlFor="">Description du produit</label>
+          <div className="flex-col hidden md:hidden lg:flex">
+            <label
+              htmlFor=""
+              className="text-center sm:text-center md:text-start"
+            >
+              Product description
+            </label>
             <textarea
               className="rounded-xl text-secondary px-2 py-1 resize-none h-28 outline-none focus:border focus:border-orange-600"
               placeholder="Saisissez la description ici"
@@ -142,11 +167,11 @@ export const FormQuote = ({ beaconDetail, closeModal }: FormQuoteProps) => {
         <input type="hidden" name="beaconIdType" value={beaconDetail.id} />
         <button
           type="submit"
-          className="bg-orange-600 sm:hidden flex items-center justify-center rounded-3xl py-2 px-4 text-primary font-montserrat font-semibold hover:transform hover:scale-110"
+          className="bg-orange-600 sm:hidden md:block flex items-center justify-center rounded-3xl py-2 px-4 text-primary font-montserrat font-semibold hover:transform hover:scale-110"
         >
           Request a quote
         </button>
       </form>
-    </div>
+    </motion.div>
   );
 };
